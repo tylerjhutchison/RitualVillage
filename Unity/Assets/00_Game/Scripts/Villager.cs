839dbcd6674920a4d36cea871f4949737e889f7f
Edwon <edwonia@gmail.com>
2016-01-29 21:47:26 -0800

    villager BEGINS

---
+++ Unity/Assets/00_Game/Scripts/Villager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Villager : MonoBehaviour
+{
+
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+	
+	}
+
+}
---
+++ Unity/Assets/00_Game/Scripts/VillagerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent( typeof(Villager) )]
+public class VillagerController : MonoBehaviour
+{
+    public Villager villagerPrefab;
+
+    public Villager[] villagers;    
+	
+	void Start ()
+    {
+
+        //create all the villagers
+        //create a char of chars
+        for (char c = 'A'; c <= 'Z'; c++)
+        {
+            //do something with letter
+            //print(c); 
+            Villager myVillager = Instantiate(villagerPrefab,Vector3.zero, Quaternion.identity) as Villager;
+        }
+        //for each letter of the alphabet, create a villager
+
+    }
+	
+	void Update ()
+    {
+	
+	}
+}
