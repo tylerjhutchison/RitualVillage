b0cdd73acc7f8d3fc800da4a6a9e360c2d1db31e
Edwon <edwonia@gmail.com>
2016-01-30 15:53:20 -0800

    lots of villager art stuff

---
+++ Unity/Assets/00_Game/Scripts/VillagerAnimation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VillagerAnimation : MonoBehaviour
+{
+    public Transform headJoint;
+
+	void Start ()
+    {
+
+    }
+	
+	void Update ()
+    {
+        //headJoint.right = -(headJoint.position - Camera.main.transform.position);
+        //headJoint.Rotate(Vector3.right, 90);
+        //headJoint.Rotate(Vector3.forward, 180);
+
+        headJoint.LookAt(Camera.main.transform);
+    }
+}
