1fbb85d3cfa720605e14270791321f6dbad1c4f1
Tyler J. Hutchison <tylerjhutchison@gmail.com>
2016-01-31 17:17:52 -0800

    Health Bar, Intro Banner, More Dancers
    I added in a simple health bar that will display how close you are do death by not dancing enough.
    
    There is a really crappy banner that flashes on screen before you start.
    
    After 30 seconds of playing the back row will jump in. This would be nicer if we spaced it out by adding 2 at a time.

--- Unity/Assets/00_Game/Scripts/GameUI.cs
+++ Unity/Assets/00_Game/Scripts/GameUI.cs
@@ -7,9 +7,41 @@ public class GameUI : MonoBehaviour {
 
 	public Image fadePlane;
 	public GameObject gameOverUI;
+	public GameObject introBanner;
+	public RectTransform healthBar;
+
+	VillagerController controller;
 
 	void Start () {
-		FindObjectOfType<VillagerController> ().OnGameOver += OnGameOver;
+
+		controller = FindObjectOfType<VillagerController> ();
+		controller.OnGameOver += OnGameOver;
+		controller.OnStartGame += OnStartGame;
+	}
+
+	void OnStartGame (){
+		StartCoroutine (Flash (Color.black, Color.clear, 1));
+	}
+
+	void Update (){
+		//health percent is the inverse of x/60  (60 - x) / 60
+		float healthPercent = (60f - controller.totalLackOfFaith) /60f;
+		healthBar.localScale = new Vector3 (healthPercent, 1, 1);	
+	}
+
+	IEnumerator Flash(Color from, Color to, float time) {
+		float speed = 1 / time;
+		float percent = 0;
+
+		introBanner.SetActive (true);
+
+		while (percent < 1) {
+			percent += Time.deltaTime * speed;
+			fadePlane.color = Color.Lerp (from, to, percent);
+			yield return null;
+		}
+		introBanner.SetActive (false);
+
 	}
 
 	void OnGameOver (){
